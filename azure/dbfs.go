9495e81397f27658a9a2b91c4a386dbfcb1c9d78
Jean-Louis Tournay <jeanlouis.tournay@gmail.com>
2020-04-16 09:00:11 +0400

    change import

--- azure/dbfs.go
+++ azure/dbfs.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/xinsnake/databricks-sdk-golang/azure/models"
+	"github.com/jltournay/databricks-sdk-golang/azure/models"
 )
 
 // DbfsAPI exposes the DBFS API
--- azure/jobs.go
+++ azure/jobs.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/xinsnake/databricks-sdk-golang/azure/models"
+	"github.com/jltournay/databricks-sdk-golang/azure/models"
 )
 
 // JobsAPI exposes Jobs API endpoints
